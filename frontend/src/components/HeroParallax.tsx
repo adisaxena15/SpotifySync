c6474062541ee42377933f9979b5af6a58722991
Aditya Saxena <143660798+adisaxena15@users.noreply.github.com>
2024-08-08 17:18:43 +0530

    fixed styling

--- frontend/src/components/HeroParallax.tsx
+++ frontend/src/components/HeroParallax.tsx
@@ -44,7 +44,7 @@ export const HeroParallax = ({gallery}: {
     return (
         <div
             ref={ref}
-            className="h-[200vh] py-40 overflow-hidden antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+            className="h-[250vh] py-40 overflow-hidden antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
         >
             <Header />
             <motion.div
